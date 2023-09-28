978a1603348d5f1fa3d03cda43e043e8386b3c8f
yamachan0625 <napoli.080625@gmail.com>
2023-09-28 19:47:06 +0900

    CI修正

--- TaskManagementDomain/src/Infrastructure/PostgreSQL/scripts/run-integration.sh
+++ TaskManagementDomain/src/Infrastructure/PostgreSQL/scripts/run-integration.sh
@@ -7,7 +7,7 @@ $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '3. DBが起動しました'
 echo '4.DBのマイグレーションを行います'
 npx prisma migrate dev --name init --schema='./src/Infrastructure/PostgreSQL/prisma/schema.prisma'
 echo '5.テストを開始します'
-jest --watch --runInBand /Infrastructure/PostgreSQL
+jest --runInBand /Infrastructure/PostgreSQL
 echo '6.DBを停止します'
 docker-compose -f docker-compose.test.yml rm -fsv testdb
 echo '6.テストが正常に終了しました'
